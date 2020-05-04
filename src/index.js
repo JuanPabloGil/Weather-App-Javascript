1a794b5f3c94935d92359cbfae2e3967d018b1bc
Juan Pablo <jpablomgil@gmail.com>
2020-05-04 15:51:55 -0500

    Adding function to convert kelvin to c and f showing it on dom

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 let  searchInput = document.querySelector('.searchInput');
 let  searchButton = document.querySelector('.searchButton');
+let name = document.querySelector('.name');
+let weather = document.querySelector('.weather');
 
+function kelvinToCelcius(temp){
+  temp = (temp - 273.15).toFixed(2) + ' C°';
+  return temp;
+}
+
+function kelvinToFahrenheit(temp){
+  temp = ((((temp - 273.15) * 9) / 5) + 32 ).toFixed(2) + ' F°';
+  return temp;
+}
 
 searchButton.addEventListener('click', () => {
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=436dd24bce78a2c523210e1f077bf18b`)
@@ -11,7 +22,8 @@ searchButton.addEventListener('click', () => {
 
     .then(function(data){
       console.log(data);
-
+      name.innerHTML = data.name;
+      weather.innerHTML = kelvinToFahrenheit(data.main.temp) ;
     })
 
     .catch(err => alert('WRONG CITY NAME!'))
